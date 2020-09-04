a33aa1ef1a7af5f716ccb93125d001abe7c4be1b
Takumi Endo <endotaku0608@gmail.com>
2020-09-04 04:58:54 +0000

    新規登録ページ、ログインページ編集#3

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '新規登録機能' do
+    it 'ユーザー新規登録できること' do
+      user = User.new(
+        name: '田中太郎',
+        email: 'exsample@gmail.com',
+        password: '123456'
+      )
+
+      expect(user).to be_valid
+
+      user.save
+
+      add_user = User.find(1)
+      expect(add_user.name).to eq('田中太郎')
+      expect(add_user.email).to eq('exsample@gmail.com')
+    end
+  end
 end
