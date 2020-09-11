917c6bac514dc5cf4b42522bfb3359a2ac254e89
Takumi Endo <endotaku0608@gmail.com>
2020-09-11 12:36:32 +0000

    フォローユーザー一覧ページ作成#44

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,4 +26,16 @@ class UsersController < ApplicationController
       end
     end
   end
+
+  def followings
+    @user = User.find(params[:id])
+    @users = @user.followings
+    render 'show_follow'
+  end
+
+  def followers
+    @user = User.find(params[:id])
+    @users = @user.followers
+    render 'show_follower'
+  end
 end
