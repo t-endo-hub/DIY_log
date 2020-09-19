a85f8c450e86bad92040995d18249950f6c00693
Takumi Endo <endotaku0608@gmail.com>
2020-09-19 08:21:54 +0000

    テスト追加

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -8,7 +8,7 @@ class HomeController < ApplicationController
   end
 
   def new_guest
-    user = User.find_or_create_by!(name: 'ゲストユーザー', email: 'guest@example.com') do |user|
+    user = User.find_or_create_by!( name: 'ゲストユーザー', email: 'guest@example.com' ) do |user|
       user.password = SecureRandom.urlsafe_base64
     end
     sign_in user
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+
   attachment :image
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
