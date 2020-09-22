800cd844a6ebccff04b4c0eb82947b746a514579
Takumi Endo <endotaku0608@gmail.com>
2020-09-22 08:51:27 +0000

    DM一覧ページレイアウト調整

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -7,6 +7,18 @@ class RoomsController < ApplicationController
     redirect_to "/rooms/#{@room.id}"
   end
 
+  def index
+    @rooms = Room.all
+    @user = current_user
+    @currentEntries = current_user.entries
+    myRoomIds = []
+    @currentEntries.each do | entry |
+      myRoomIds << entry.room.id
+    end
+    @anotherEntries = Entry.where(room_id: myRoomIds).where('user_id != ?', @user.id)
+
+  end
+
   def show
     @room = Room.find(params[:id])
     if Entry.where(user_id: current_user.id, room_id: @room.id).present?
