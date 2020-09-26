bda42f21acd5aabba91b88e3aaaf28c738ef202e
Takumi Endo <endotaku0608@gmail.com>
2020-09-26 11:40:40 +0000

    デプロイ設定

--- db/migrate/20200905113445_create_relationships.rb
+++ db/migrate/20200905113445_create_relationships.rb
@@ -1,8 +1,8 @@
 class CreateRelationships < ActiveRecord::Migration[5.0]
   def change
     create_table :relationships do |t|
-      t.references :user, foreign_key: true
-      t.references :follow, foreign_key: { to_table: :users }
+      t.references :user, type: :bigint, foreign_key: true
+      t.references :follow, type: :bigint, foreign_key: { to_table: :users }
 
       t.timestamps
 
