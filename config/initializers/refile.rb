4f32a243183c4c9316a2ae2f38e12af70959707c
Takumi Endo <endotaku0608@gmail.com>
2020-10-12 09:21:47 +0000

    画像保存先変更

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -5,7 +5,7 @@ if Rails.env.production? # 本番環境の場合はS3へアップロード
     access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキーID
     secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], # シークレットアクセスキー
     region: 'ap-northeast-1', # リージョン
-    bucket: 'pf-buket', # バケット名
+    bucket: 'diylog-bucket', # バケット名
   }
   Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
   Refile.store = Refile::S3.new(prefix: 'store', **aws)
