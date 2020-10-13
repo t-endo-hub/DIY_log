340ccd0032c2afb5595ea49b014d44e4c9ab9da8
Takumi Endo <endotaku0608@gmail.com>
2020-10-13 02:55:28 +0000

    画像をresizeして別バケットに保存

--- config/initializers/refile.rb
+++ config/initializers/refile.rb
@@ -1,12 +1,9 @@
 require 'refile/s3'
-
-if Rails.env.production? # 本番環境の場合はS3へアップロード
-  aws = {
-    access_key_id: ENV['S3_ACCESS_KEY_ID'], # アクセスキーID
-    secret_access_key: ENV['S3_SECRET_ACCESS_KEY'], # シークレットアクセスキー
-    region: 'ap-northeast-1', # リージョン
-    bucket: 'diylog-bucket', # バケット名
-  }
-  Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
-  Refile.store = Refile::S3.new(prefix: 'store', **aws)
-end
+aws = {
+  access_key_id: ENV['AWS_ACCESS_KEY_ID'],
+  secret_access_key: ENV['AWS_SECRET_ACCESS_KEY'],
+  region: ENV['AWS_REGION'],
+  bucket: ENV['AWS_S3_BUCKET_NAME']
+}
+Refile.cache = Refile::S3.new(prefix: 'cache', **aws)
+Refile.store = Refile::S3.new(prefix: 'store', **aws)
