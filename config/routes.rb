0c1166f602039badfdd56926dc0a6802aa171ef3
Takumi Endo <endotaku0608@gmail.com>
2020-10-16 06:06:12 +0000

    投稿編集できないバグ修正

--- config/routes.rb
+++ config/routes.rb
@@ -34,7 +34,7 @@ Rails.application.routes.draw do
       end
       resource :likes, only: %i(create destroy)
       resources :comments, only: %i(create destroy)
-      resources :recipes, only: %i(new index show create destroy)
+      resources :recipes, only: %i(new index show create update destroy)
       resources :materials, only: %i(create destroy)
       resources :items, only: %i(create destroy)
       resources :orders, only: %i(new create) do
